acc97b55f2f88b99f1f442fcf9b2661f4015e92e
Cyrus-M2 <devsben345@gmail.com>
2025-06-29 16:16:06 +0300

    importing mui-material

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { Typography } from "@mui/material"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h2>Hello React</h2>
-    </>
+    <div>
+      <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+      <Typography variant="h5">Heading 5</Typography>
+      <Typography variant="h6">Heading 6</Typography>
+    </div>
   )
 }
 
