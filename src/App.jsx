69cb02f54d35e4b18dd9bc3591a2cf10001f884a
Cyrus-M2 <devsben345@gmail.com>
2025-06-29 16:19:50 +0300

    subtitles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
 
   return (
+    // Heading 
     <div>
       <Typography variant="h1">Heading 1</Typography>
       <Typography variant="h2">Heading 2</Typography>
@@ -11,7 +12,13 @@ function App() {
       <Typography variant="h4">Heading 4</Typography>
       <Typography variant="h5">Heading 5</Typography>
       <Typography variant="h6">Heading 6</Typography>
+
+    {/* subtitles */}
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
     </div>
+
+    
   )
 }
 
