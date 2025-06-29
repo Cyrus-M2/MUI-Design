44aec18519b1f80325a7f08daecb1474d30f5583
Cyrus-M2 <devsben345@gmail.com>
2025-06-29 17:25:26 +0300

    button sizes

--- src/Muibutton.jsx
+++ src/Muibutton.jsx
@@ -2,7 +2,7 @@ import { Button, Stack } from '@mui/material';
 
 const Muibutton = () => {
   return (
-    <Stack direction="row" spacing={4}>
+    <Stack direction="column" spacing={4} >
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outline</Button>
       <Button variant="text">Text</Button>
@@ -11,7 +11,23 @@ const Muibutton = () => {
       <Button variant="text" href="https://www.google.com">
       Visit Google
     </Button>
-    
+
+    {/* Button Colors﻿ */}
+    <Stack direction="column" spacing={4}>
+      <Button variant="contained" color="primary">Primary</Button>
+      <Button variant="contained" color="secondary">Secondary</Button>
+      <Button variant="contained" color="error">Error</Button>
+      <Button variant="contained" color="info">Info</Button>
+      <Button variant="contained" color="warning">Warning</Button>
+      <Button variant="contained" color="success">Success</Button>
+    </Stack>
+
+    {/* Button size﻿ */}
+    <Stack direction="column" display="block" spacing={4}>
+      <Button variant="contained" size="small">Small</Button>
+      <Button variant="contained" size="medium">Medium</Button>
+      <Button variant="contained" size="large">Large</Button>
+    </Stack>
     </Stack>
     
   )
